052f53eba3f4038362632473aade37f7f430d1c7
Demian Neidetcher <demian0311@gmail.com>
2016-01-12 19:46:05 -0600

    stmt vs exp

---
+++ src/test/java/com/neidetcher/fp/p03/StatementsVsExpressions.java
@@ -0,0 +1,35 @@
+package com.neidetcher.fp.p03;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StatementsVsExpressions {
+
+    private final long annualPayment = 40_000;
+    private final int age = 66;
+
+    /**
+     * Statements do not resolve to a value.
+     */
+    @Test
+    public void statement() {
+        long monthlyBenefit;
+        if (age >= 65) {
+            monthlyBenefit = annualPayment / 12;
+        } else {
+            monthlyBenefit = 0;
+        }
+        assertEquals(3_333, monthlyBenefit);
+    }
+
+    /**
+     * Expressions resolve to a value.
+     */
+    @Test
+    public void expression() {
+        final long monthlyBenefit = age >= 65? annualPayment / 12: 0;
+        assertEquals(3_333, monthlyBenefit);
+    }
+
+}
